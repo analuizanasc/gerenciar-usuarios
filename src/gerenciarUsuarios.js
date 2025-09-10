7684ac0c4400a0623a8ddeef56cdc6b3ba3371dc
Ana Luiza <analuiza_04@hotmail.com>
2025-09-09 22:58:08 -0300

    mudei a lista com nome de  usuarios e criei objeto

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,11 +1,28 @@
-const usuarios = ['ana', 'maria', 'joana', 'carla'];
+const usuarios = [
+    {
+        nome: 'ana',
+        email: 'ana@email.com'
+    },
+    {
+        nome: 'maria',
+        email: 'maria@email.com'
+    },
+    {
+        nome: 'joana',
+        email: 'joana@email.com'
+    },
+    {
+        nome: 'carla',
+        email: 'carla@email.com'
+    }
+]
 
 function retornarUsuarios() {
     return usuarios;
 }
 
-function adicionarUsuarios(nome) {
-    usuarios.push(nome)
+function adicionarUsuarios(usuario) {
+    usuarios.push(usuario)
 }
 
 module.exports = {
--- test/genrenciarUsuarios.test.js
+++ test/genrenciarUsuarios.test.js
@@ -1,10 +1,19 @@
-const {adicionarUsuarios, retornarUsuarios} = require('../src/gerenciarUsuarios');
+const { adicionarUsuarios, retornarUsuarios } = require('../src/gerenciarUsuarios');
 const { expect } = require('chai');
 
-describe ('Testar as funções de gerenciamento de Usuários', function(){
-    it('Validar que posso adicionar um novo nome de usuário na lista', function(){
-        adicionarUsuarios('Zefa')
+describe('Testar as funções de gerenciamento de Usuários', function () {
+    it('Validar que posso adicionar um novo nome de usuário na lista', function () {
+        adicionarUsuarios({
+            nome: 'Zefa',
+            email: 'Zefa@email.com'
+        })
         const listaUsuarios = retornarUsuarios()
-        expect(listaUsuarios.at(-1)).to.equal('Zefa')
-    })
+        //expect(listaUsuarios.at(-1).nome).to.equal('Zefa')
+        //to.deep.eql - > comparar dois objetos, listas
+        //equal -> compara só um valor 
+        expect(listaUsuarios.at(-1)).to.eql({
+            nome: 'Zefa',
+            email: 'Zefa@email.com'
+        })
+    });
 })
